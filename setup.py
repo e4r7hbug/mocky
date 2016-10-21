5aff38739cff73ef4f67d2a77c8c1ab90f55c1c6
Nate Tangsurat <e4r7hbug@gmail.com>
2016-10-21 09:01:06 -0500

    fix: Make command to level mapping

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 """Mocky installer."""
 from setuptools import find_packages, setup
 
+mocked_commands = {
+    'cc': 'critical',
+    'cp': 'info',
+    'gcc': 'critical',
+    'install': 'error',
+    'rm': 'debug',
+}
+
 setup(
     name='mocky',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=['coloredlogs'],
     entry_points={
-        'console_scripts': [
-            'cc = mocky.__main__:critical',
-            'cp = mocky.__main__:info',
-            'gcc = mocky.__main__:critical',
-            'install = mocky.__main__:error',
-            'mocky = mocky.__main__:main',
-            'rm = mocky.__main__:debug',
-        ],
+        'console_scripts':
+        ['{0} = mocky.__main__:{1}'.format(command, level) for command, level in mocked_commands.items()],
     }, )
