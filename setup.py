ccd07803c83e0b6bb5ed01c7a3e77f7cccc010d3
Nate Tangsurat <e4r7hbug@gmail.com>
2016-10-21 01:54:12 -0500

    feat: Stub commands

--- setup.py
+++ setup.py
@@ -7,4 +7,12 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=['coloredlogs'],
-    entry_points={'console_scripts': ['mocky = mocky.__main__:main'], }, )
+    entry_points={
+        'console_scripts': [
+            'mocky = mocky.__main__:main',
+            'rm = mocky.__main__:debug',
+            'cp = mocky.__main__:info',
+            'install = mocky.__main__:error',
+            'gcc = mocky.__main__:critical',
+        ],
+    }, )
