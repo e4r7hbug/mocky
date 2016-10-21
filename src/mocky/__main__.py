3032b14cef36bf4f78e24b11e5f149f04f0898a8
Nate Tangsurat <e4r7hbug@gmail.com>
2016-10-21 01:53:46 -0500

    feat: Stub out logged commands

--- src/mocky/__main__.py
+++ src/mocky/__main__.py
@@ -1,8 +1,27 @@
 """Mocked POSIX commands."""
+import pathlib
+import sys
+import functools
+
 import coloredlogs
 
 coloredlogs.install(level='DEBUG')
 LOG = coloredlogs.logging.getLogger(__package__)
+
+
+def command_squash():
+    """Squash command and arguments together."""
+    command, *args = sys.argv
+    command_name = pathlib.Path(command).name
+    return ' '.join([command_name] + args)
+
+
+debug = functools.partial(LOG.debug, '%s', command_squash())
+info = functools.partial(LOG.info, '%s', command_squash())
+warning = functools.partial(LOG.warning, '%s', command_squash())
+error = functools.partial(LOG.error, '%s', command_squash())
+critical = functools.partial(LOG.critical, '%s', command_squash())
+fatal = functools.partial(LOG.fatal, '%s', command_squash())
 
 
 def main():
